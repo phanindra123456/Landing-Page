019178b94d21cd30a99ff4b6d16158920edfa2a1
phanindra123456 <phanindravulli123@gmail.com>
2024-03-07 21:05:49 +0530

    Initial commit

--- src/Components/FoodMenu.js
+++ src/Components/FoodMenu.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import masala from "../Assets/masala.jpg";
 import paneer from "../Assets/paneer.jpg";
 
@@ -19,38 +18,22 @@ const FoodMenuItem = ({ name, description, image, price }) => (
   </div>
 );
 
-const checkout = () => {
-  const foodMenuItems = [
-    {
-      name: "Masala Dosa",
-      description: "Crispy rice crepe filled with spiced potatoes.",
-      image: "masala.jpg",
-      price: 80.0,
-    },
-  ];
-  fetch(" http://localhost:5000/create-checkout-session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    mode: "cors",
-    body: JSON.stringify({
-      items: [{ id: 1, price: foodMenuItems[0].price, name: foodMenuItems[0].name }],
-    }),
-  })
-    .then((res) => {
-      if (res.ok) return res.json();
-      return res.json().then((json) => Promise.reject(json));
-    })
-    .then(({ url }) => {
-      window.location = url;
-    })
-    .catch((e) => {
-      console.log(e.error);
+const checkout = async () => {
+  try {
+    const response = await fetch("http://localhost:5000/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
     });
-  console.log(foodMenuItems[0].price);
-  console.log(foodMenuItems[0].name);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 };
+
 
 const FoodMenu = () => {
   const foodMenuItems = [
